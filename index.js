085aeec43ceaa82750d418f76bee749daaf3aaac
alvinkenyagah <alvin47k@gmail.com>
2023-04-05 13:41:25 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+// declare customerName to be bob in global scope
+var customerName = 'bob';
+
+// upperCaseCustomerName() function
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+// setBestCustomer() function
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+// overwriteBestCustomer() function
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+// changeLeastFavoriteCustomer() function (using const)
+const leastFavoriteCustomer = 'someone';
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'someone else'; // this should throw an error
+}
